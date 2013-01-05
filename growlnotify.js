b9f9e210fbdd68c9664ad8c74df63c88da806e3a
Uli Preuss <me@ulipreuss.eu>
2013-01-05 23:23:18 +0100

    ie fix for loading styles

--- growlnotify.js
+++ growlnotify.js
@@ -6,7 +6,7 @@
  * Modified 2011 by Uli Preuss
  */
 
-(function(global) {
+(function(global, doc) {
 
   var growl = {
 
@@ -38,20 +38,26 @@
         position,
         fragment,
         popup,
-        sticky,
+        persistent,
         cache = ''
       ;
 
       popups[id] = {};
-      sticky = popups[id].sticky = config["sticky"] !== null ? config["sticky"] : false;
+      persistent = popups[id].persistent = config["persistent"] !== null ? config["persistent"] : false;
       popups[id].timeout = config["timeout"] !== undefined ? parseInt(config["timeout"], 10) : 500;
       position = popups[id].position = config["position"] || 'bottom right';
       popups[id].opacity = 0;
 
       if (typeof wrapper[position] === 'undefined') {
-        wrapper[position] = document.createElement("div");
+        wrapper[position] = doc.createElement("div");
         fragment = wrapper[position];
-        fragment.className = "gn";
+        try {
+          fragment.className = "gn";
+        } catch(e) {
+           // This executes in IE7,
+           // but not IE8, regardless of mode
+          fragment.setAttribute("className", "gn");
+        }
         if (/right/.test(position)) {
           fragment.style.right = '0';
         } else if (/left/.test(position)) {
@@ -62,11 +68,11 @@
         } else if (/bottom/.test(position)) {
           fragment.style.bottom = '0';
         }
-        document.body.appendChild(fragment);
+        doc.getElementsByTagName('body')[0].appendChild(fragment);
       }
 
-      popup = popups[id].notice = document.createElement("div");
-      if (sticky) {
+      popup = popups[id].notice = doc.createElement("div");
+      if (persistent) {
         cache += "<span class='gn-close' id='gn-close-" + id + "'></span>";
       }
       if (gn_icon || icon) {
@@ -79,14 +85,14 @@
         cache += "<div class='gn-body'>" + body + "</div>";
       }
       popup.innerHTML = cache;
-      popup.setAttribute('class', 'gn-wrapper');
+      popup.className = "gn-wrapper";
       popup.id = "gn-wrapper_" + id;
       popup.style.opacity = "0";
       popup.style.filter = "alpha(opacity = 0)";
       wrapper[position].appendChild(popup);
-      if (sticky) {
+      if (persistent) {
         var self = this;
-        var closeButton = document.getElementById("gn-close-" + id);
+        var closeButton = doc.getElementById("gn-close-" + id);
         closeButton.onclick = function() {
           self.close(this, "slow");
         };
@@ -97,7 +103,7 @@
 
     close: function(obj, speed) {
       var id = obj.id.split('-')[2];
-      if (document.getElementById("gn-close-" + id)) {
+      if (doc.getElementById("gn-close-" + id)) {
         this.hide(id, speed);
       }
     },
@@ -119,7 +125,7 @@
         timer = setTimeout("growl.show('" + id + "')", 75);
       } else {
         clearTimeout(timer);
-        if (!popups[id].sticky) {
+        if (!popups[id].persistent) {
           setTimeout("growl.hide('" + id + "')", popups[id].timeout);
         }
       }
@@ -146,18 +152,19 @@
       }
     },
 
-    add_growl_styles: (function() {
-      var style = document.createElement('style');
-      style.type = 'text/css';
+    add_growl_styles: function() {
+      var inline_style = doc.createElement('style');
+      inline_style.type = 'text/css';
       var content = ' ' +
       '.gn { ' +
-      '  position: fixed; padding: 20px 20px 0px; min-width: 250px; max-width: 350px; ' +
+      '  position: absolute; padding: 20px 20px 0px; min-width: 250px; max-width: 350px; *width: 350px;' +
       '}' +
       '.gn-wrapper {' +
       '  width: auto; min-height: 27px; display: block; background-color: #242321; margin-bottom: 15px;' +
       '  font-family: Lucida Grande,Lucida Sans Unicode,Geneva,Verdana,sans-serif; padding: 0px 10px 10px;' +
       '  box-shadow: 0px 0px 7px #000; -moz-box-shadow: 0px 0px 7px #000; -webkit-box-shadow: 0px 0px 7px #000;' +
-      '  border-radius: 9px; -moz-border-radius: 9px; -webkit-border-radius: 9px; position: relative; border: thin solid #AAA' +
+      '  -moz-border-radius: 9px; -webkit-border-radius: 9px; border-radius: 9px; position: relative; ' +
+      '  border: thin solid #AAA' +
       '}' + 
       '.gn-close {' +
       '   position: absolute; right: -10px; top: -11px; cursor: pointer; color: #EFEFEF;' +
@@ -172,22 +179,21 @@
       '  position: relative; top: 7px; width: 24px; height: 24px; margin: 0px 6px 0px 0px;' +
       '}' + 
       '.gn-head {' +
-      '  display: inline-block; font-size: 13px; word-wrap: break-word; color: #FFF; line-height: 120%; margin: 9px 0px 0px 0px;' +
+      '  display: inline-block; *display: inline; font-size: 13px; word-wrap: break-word; color: #FFF; line-height: 120%; margin: 9px 0px 0px 0px;' +
       '}' + 
       '.gn-body {' +
-      '  padding-top: 12px; font-size: 12px; color: #FEFEFE; line-height: 120%;' +
+      '  padding-left: 30px; padding-right: 10px; padding-top: 12px; font-size: 12px; color: #FEFEFE; line-height: 120%;' +
       '}';
 
-      if (style.styleSheet) {   // IE
-          style.styleSheet.cssText = content;
+      if (inline_style.styleSheet) {   // IE
+          inline_style.styleSheet.cssText = content;
       } else {                // the world
-          style.appendChild(document.createTextNode(content));
+          inline_style.appendChild(doc.createTextNode(content));
       }
-
-      document.getElementsByTagName('head')[0].appendChild(style);
+      doc.getElementsByTagName('head')[0].appendChild(inline_style);
       
-    } ())
-
+    }
+    
   };
 
   // Expose growl to the global object
@@ -196,5 +202,12 @@
   // aliases for public use
   global.growl.notify = growl.notify;
   global.growl.close = growl.close;
+  global.growl.add_growl_styles = growl.add_growl_styles;
 
-}(window));
+  if (window.addEventListener) {
+      window.addEventListener('DOMContentLoaded', growl.add_growl_styles, false);
+  } else {
+      window.attachEvent('onload', growl.add_growl_styles);
+  }
+
+}(window, document));
